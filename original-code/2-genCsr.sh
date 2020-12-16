59eb093835b0d6de20aed2f76921a68026aa4e44
Patrick A. Hoover Jr <pathooverjr@gmail.com>
2020-12-16 13:18:59 -0600

    Change folder name

---
+++ original-code/2-genCsr.sh
@@ -0,0 +1,6 @@
+. ./config.sh
+
+mkdir -p $KEY_PATH
+
+#gen csr using java keytool
+keytool -certreq -alias $CLUSTER_NAME -file $KEY_PATH/$CLUSTER_NAME.csr -keystore $KEY_PATH/$PRIVATE_KEYSTORE -storepass $KS_PASSWD
